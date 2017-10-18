c9478faca2e97ac8cc9cf5559df9ea092af0c283
heart5-asus <baiyefeng@gmail.com>
2017-10-18 16:13:26 +0800

    验证客户档案和产品档案的周全性；引入包文件化

---
+++ imp4nb.py
@@ -0,0 +1,6 @@
+import pandas as pd, sqlite3 as lite, matplotlib.pyplot as plt, numpy as np,calendar as cal, random as rd
+from pylab import *
+
+# plot中显示中文
+mpl.rcParams['font.sans-serif'] = ['SimHei']
+mpl.rcParams['axes.unicode_minus'] = False
