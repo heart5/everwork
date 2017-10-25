34bcc75f7a02e1db4bf3fd6e80102fbda3d2e683
heart5-asus <baiyefeng@gmail.com>
2017-10-26 00:56:55 +0800

    判断子目录是否存在，没有则建立。客户档案维护处理有问题，尚未解两次及以上修改编码正确处理的问题。

--- imp4nb.py
+++ imp4nb.py
@@ -1,7 +1,13 @@
-import pandas as pd, sqlite3 as lite, matplotlib.pyplot as plt, numpy as np,calendar as cal, random as rd
+import pandas as pd, sqlite3 as lite, matplotlib.pyplot as plt, numpy as np,calendar as cal, random as rd, os
 import re as re
 from pylab import *
 
 # plot中显示中文
 mpl.rcParams['font.sans-serif'] = ['SimHei']
 mpl.rcParams['axes.unicode_minus'] = False
+
+if not os.path.exists('data'):
+    os.mkdir('data')
+
+if not os.path.exists('img'):
+    os.mkdir('img')
