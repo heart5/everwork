40ba372d42136b92493e54471aa2115e7b35e7a5
heart5-asus <baiyefeng@gmail.com>
2018-01-04 11:01:51 +0800

    调校参数，这次是193次到坎了

--- everfunc.py
+++ everfunc.py
@@ -96,8 +96,9 @@ inifilepath = 'data\\everwork.ini'
 cfp.read(inifilepath,encoding='utf-8')
 ENtimes = cfp.get('evernote','apicount')
 apilasttime = pd.to_datetime(cfp.get('evernote','apilasttime'))
-apilasttimehouzhengdian = pd.to_datetime((apilasttime+datetime.timedelta(hours=1)).strftime('%Y-%m-%d %H:00:00'))
+apilasttimehouzhengdian = pd.to_datetime((apilasttime+datetime.timedelta(hours=1)).strftime('%Y-%m-%d %H:02:00'))
 if datetime.datetime.now() > apilasttimehouzhengdian:
+    time.sleep(60)
     ENtimes = 0
 
 
@@ -105,10 +106,10 @@ def jiayi():
     global ENtimes, cfp, inifilepath
     ENtimes += 1
     log.debug('动用了Evernote API %d 次……' % ENtimes)
-    if ENtimes > 200:
+    if ENtimes > 190:
         now = datetime.datetime.now()
         zhengdian = pd.to_datetime('%4d-%2d-%2d %2d:00:00' % (now.year, now.month, now.day, now.hour+1))
-        sleep_seconds = (zhengdian - now).seconds + 30
+        sleep_seconds = (zhengdian - now).seconds + 60
         log.info('Evernote API 调用已达%d次，休息%d秒，待%s再开干……' % (ENtimes, sleep_seconds, str(zhengdian)))
         time.sleep(sleep_seconds)
         ENtimes = 0
