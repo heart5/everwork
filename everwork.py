98ae8f35398677d55f9f497c0f598b1e847bf2c3
heart5-asus <baiyefeng@gmail.com>
2018-01-09 03:43:09 +0800

    学会了todo的增加方法（注释用todo即可）；为了更灵活的处理数据，采用了查询语句参数化的方式。

--- everwork.py
+++ everwork.py
@@ -1,9 +1,8 @@
-#
 # encoding:utf-8
-#
-# 用evernote作为工作平台，通过Python链接整理各种工作数据，呈现给各个相关岗位。.
-#
-#
+"""
+用evernote作为工作平台，通过Python链接整理各种工作数据，呈现给各个相关岗位。.
+"""
+
 from imp4nb import *
 import evernote.edam.userstore.constants as UserStoreConstants
 import evernote.edam.type.ttypes as Types
@@ -11,6 +10,7 @@ from evernote.api.client import EvernoteClient
 from noteweather import weatherstat #调用同目录下其他文件（py）中的函数
 from notewarehouse import *
 from notedispatch import *
+from notesaledetails import *
 
 # thistime = '当前运行时间：' + str(int(time.time())) + '\t' + time.strftime("%Y-%m-%d %H:%M:%S",
 #                                                                     time.localtime(time.time()))
@@ -48,7 +48,7 @@ note_store = get_notestore(token)
 #     throws Errors.EDAMUserException, Errors.EDAMSystemException, Errors.EDAMNotFoundException
 
 # note = note_store.getNote(token,'296f57a3-c660-4dd5-885a-56492deb2cee', True, True, True,True)
-# jiayi()
+# evernoteapijiayi()
 # print(note.content)
 # resources = note.resources
 # print(len(resources))
@@ -59,7 +59,7 @@ note_store = get_notestore(token)
 
 # printnoteattributeundertoken(note)
 
-
+# todo 一体化目录构建
 nbfbdf = readinisection2df(cfp,'guidfenbunb','销售业绩图表')
 for aa in nbfbdf.index:
     cpath = 'img\\'+aa
@@ -68,9 +68,9 @@ for aa in nbfbdf.index:
         log.debug('目录《' + cpath + '》被创建')
 
 cnx = lite.connect('data\\quandan.db')
-# dataokay(cnx)
-# pickstat(note_store,cnx, '1c0830d9-e42f-4ce7-bf36-ead868a55eca','订单配货统计图')
-brandlist = ['兴仁居', '有友','咚咚', '知欣', '兰花恋人',
+dataokay(cnx)
+# pickstat(token, note_store, cnx, '1c0830d9-e42f-4ce7-bf36-ead868a55eca', '订单配货统计图', cum=True)
+brandlist = ['兰花恋人',
              '易加', '卤帝七号', '呈呈', '伍滋味', '脆马蹄', '鱼友味', '武丰', '柒柒湘', '俊媳妇', '湘寿鸭', '好媳妇',
               '香之派', '友意', '大西南', '金昌盛', '康赞', '恒的', '孙妈', '银城湘味', '刘香源', '蜀望', '口口德福',
              '威龙', '好运', '心诺',  '农尔康', '君思', '珍可惠', '红帅', '先生', '好棒美', '三友', '君妃', '火卤卤',
@@ -83,15 +83,14 @@ brandlist = ['兴仁居', '有友','咚咚', '知欣', '兰花恋人',
 # brandlist = ['津津友味','鲜多鲜','丽芝士','非凡','鲜客','童年时代','麦小呆','渔米之湘','U品部落',
 #              '抓鱼的猫','旭东','创食人','']
 # brandlist = ['津津友味','鲜多鲜','丽芝士','童年时代','渔米之湘','U品部落','抓鱼的猫','卫龙','创食人','']
-brandlist = ['威龙', '好运', '心诺',  '农尔康', '君思', '珍可惠', '红帅', '先生', '好棒美', '三友', '君妃', '火卤卤',
-             '花氏', '洞庭渔王', '相思', '凤凰园', '洁龙', '怡口湘', '阿林', '好巴食', '巧大娘', '老成都', '一品鱼舫',
-             '香约', '朝启', '顶牛', '湘俚味', '红叶', '无穷', '快活嘴', '花心子', '蛋大厨', '绿野香', '佳宝',
-             '湘宝王', '乡巴佬', '鑫之恋', '泰越精厨', '可可哥', '倍儿爽', '小鹏', '醉吃香', '津尝乐', '张师傅',
-             '馋大嘴', '卫龙', '凤将军', '馋大嘴', '儿时代', '爽口佳', '新丰园', '津津友味', '鲜多鲜','U品部落',
-             '光头祥', '大霸王', '丽芝士', '非凡', '鲜客', '童年时代', '麦小呆', '渔米之湘',
-             'U品部落', '抓鱼的猫', '旭东', '创食人','']
-# brandlist = []
-# brandlist = []
+# brandlist = ['威龙', '好运', '心诺',  '农尔康', '君思', '珍可惠', '红帅', '先生', '好棒美', '三友', '君妃', '火卤卤',
+#              '花氏', '洞庭渔王', '相思', '凤凰园', '洁龙', '怡口湘', '阿林', '好巴食', '巧大娘', '老成都', '一品鱼舫',
+#              '香约', '朝启', '顶牛', '湘俚味', '红叶', '无穷', '快活嘴', '花心子', '蛋大厨', '绿野香', '佳宝',
+#              '湘宝王', '乡巴佬', '鑫之恋', '泰越精厨', '可可哥', '倍儿爽', '小鹏', '醉吃香', '津尝乐', '张师傅',
+#              '馋大嘴', '卫龙', '凤将军', '馋大嘴', '儿时代', '爽口佳', '新丰园', '津津友味', '鲜多鲜','U品部落',
+#              '光头祥', '大霸王', '丽芝士', '非凡', '鲜客', '童年时代', '麦小呆', '渔米之湘',
+#              'U品部落', '抓鱼的猫', '旭东', '创食人','']
+# brandlist = ['']
 for br in brandlist:
     updatesection(cfp,'guidfenbunb',br+'kehuguidfenbu',inifilepath,token,note_store,br+'客户开发图表')
     updatesection(cfp,'guidfenbunb',br+'saleguidfenbu',inifilepath,token,note_store,br+'销售业绩图表')
@@ -102,32 +101,29 @@ for br in brandlist:
     notelxxsdf = readinisection2df(cfp,br+'saleguidleixing',br+'销售图表')
     notefbxsdf = readinisection2df(cfp,br+'saleguidfenbu',br+'销售图表')
     # print(notefbxsdf)
-    qrystr = "select 日期,strftime('%Y%m',日期) as 年月,customer.往来单位编号 as 客户编码,sum(金额) as 退货金额," \
-             "substr(customer.往来单位编号,1,2) as 区域 ,"  "substr(customer.往来单位编号,12,1) as 类型, " \
-             "product.品牌名称 as 品牌 from xiaoshoumingxi, customer, product " \
-             "where (customer.往来单位 = xiaoshoumingxi.单位全名) " \
-             "and (product.商品全名 = xiaoshoumingxi.商品全名) " \
-             "and (金额 < 0)"
-    if len(br) > 0:
-        qrystr += ' and (品牌 = \'%s\')' %br
-    qrystr += ' group by 日期,客户编码 order by 日期'
-    fenxiyueduibi(token, note_store, qrystr, '退货金额', notefbxsdf, notelxxsdf, cnx, pinpai=br, cum=True)
+
+    qrystr = "select 日期,strftime('%%Y%%m',日期) as 年月,customer.往来单位编号 as 客户编码," + \
+             'sum(金额) as %s, substr(customer.往来单位编号,1,2) as 区域 ,substr(customer.往来单位编号,12,1) as 类型, ' \
+             'product.品牌名称 as 品牌 from xiaoshoumingxi, customer, product ' \
+             'where (customer.往来单位 = xiaoshoumingxi.单位全名) and (product.商品全名 = xiaoshoumingxi.商品全名) ' \
+             '%s %s group by 日期,客户编码 order by 日期'  # % (xmclause,jineclause, brclause)
+    xiangmu = ['销售金额', '退货金额']
+    fenxiyueduibi(token, note_store, qrystr, xiangmu, notefbxsdf, notelxxsdf, cnx, pinpai=br, cum=True)
+    # fenximonthduibi(token, note_store, cnx, notefbxsdf, notelxxsdf, '金额', pinpai=br, cum=True)
 
     # notelxkhdf = ['']
     notelxkhdf = readinisection2df(cfp, br+'kehuguidleixing', br+'客户图表')
     notefbkhdf = readinisection2df(cfp, br+'kehuguidfenbu', br+'客户图表')
     # print(notefbkhdf)
-    qrystr = "select 日期,strftime('%Y%m',日期) as 年月,customer.往来单位编号 as 客户编码,count(*) as 退货客户数," \
-             "sum(金额) as 金额," \
-             "substr(customer.往来单位编号,1,2) as 区域 ,"  "substr(customer.往来单位编号,12,1) as 类型, " \
-             "product.品牌名称 as 品牌 from xiaoshoumingxi, customer, product " \
-             "where (customer.往来单位 = xiaoshoumingxi.单位全名) " \
-             "and (product.商品全名 = xiaoshoumingxi.商品全名) " \
-             "and (金额 < 0)"
-    if len(br) > 0:
-        qrystr += ' and (品牌 = \'%s\')' %br
-    qrystr += ' group by 日期,客户编码 order by 日期'
-    fenxiyueduibi(token, note_store, qrystr,'退货客户数', notefbkhdf, notelxkhdf, cnx, pinpai=br)
+    qrystr = "select 日期,strftime('%%Y%%m',日期) as 年月,customer.往来单位编号 as 客户编码," + \
+             'count(*) as %s, substr(customer.往来单位编号,1,2) as 区域 ,' \
+             'substr(customer.往来单位编号,12,1) as 类型, ' \
+             'product.品牌名称 as 品牌 from xiaoshoumingxi, customer, product ' \
+             'where (customer.往来单位 = xiaoshoumingxi.单位全名)  and (product.商品全名 = xiaoshoumingxi.商品全名) ' \
+             '%s %s group by 日期,客户编码 order by 日期'  # % (xmclause,jineclause, brclause)
+    xiangmu = ['销售客户数', '退货客户数']
+    # fenxiyueduibi(token, note_store, qrystr,xiangmu, notefbkhdf, notelxkhdf, cnx, pinpai=br)
+    # fenximonthduibi(token, note_store, '退货客户数', notefbkhdf, notelxkhdf, cnx, pinpai=br)
 
 # desclitedb(cnx)
 # swissknife(cnx)
