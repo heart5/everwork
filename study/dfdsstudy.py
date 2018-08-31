d570380c28724665f6995f12f308b2c8cc153170
heart5-huipu <baiyefeng@gmail.com>
2018-08-31 23:03:48 +0800

    DataFrame和DataSeries学习

---
+++ study/dfdsstudy.py
@@ -0,0 +1,17 @@
+# encoding:utf-8
+"""
+模块说明
+"""
+import pandas as pd
+import numpy as np
+
+
+def dfdsall():
+    sere = pd.Series([1, 2, 3, 'heart5'])
+    pass
+
+
+if __name__ == '__main__':
+    print(f'开始测试文件\t{__file__}')
+    dfdsall()
+    print('Done.测试完成。')
