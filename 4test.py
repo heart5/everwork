a8bbec3c67b716bb1bd8b635485fc9b662fe0e43
heart5 <baiyefeng@gmail.com>
2018-10-08 00:04:34 +0800

    为了测试

--- 4test.py
+++ 4test.py
@@ -1,4 +1,24 @@
 # encoding:utf-8
+# ---
+# jupyter:
+#   jupytext_format_version: '1.3'
+#   jupytext_formats: py:light
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+#   language_info:
+#     codemirror_mode:
+#       name: ipython
+#       version: 3
+#     file_extension: .py
+#     mimetype: text/x-python
+#     name: python
+#     nbconvert_exporter: python
+#     pygments_lexer: ipython3
+#     version: 3.6.6
+# ---
+
 """
 功能描述
 """
