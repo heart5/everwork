fca231dd3d40df40885ce0b14a5048d440c82fff
heart5 <baiyefeng@gmail.com>
2018-11-20 23:47:09 +0800

    autopep8 for showfont

--- etc/showzhfont.py
+++ etc/showzhfont.py
@@ -11,11 +11,11 @@ fm = FontManager()
 # print(fm.findfont(matplotlib.font_manager.FontProperties(), fontext='ttf'))
 # mat_fonts = set(f.fname for f in fm.ttflist) # matplotlib可用的字体路径列表
 # print(mat_fonts)
-mat_fonts = set(f.name for f in fm.ttflist) # matplotlib可用的字体列表
+mat_fonts = set(f.name for f in fm.ttflist)  # matplotlib可用的字体列表
 # print('*' * 10, 'matplotlib可用的字体', '*' * 10)
 # print(mat_fonts)
 output = subprocess.check_output('fc-list :lang=zh -f "%{family}\n"',
-    shell=True)   # 列出系统可用的中文字体，仅linux下可运行fc-list命令
+                                 shell=True)   # 列出系统可用的中文字体，仅linux下可运行fc-list命令
 # print(output.decode('utf-8'))
 outputsplit = output.decode('utf-8').split('\n')    # 解码bytes为str并分割
 # print '*' * 10, '系统可用的中文字体', '*' * 10
@@ -25,7 +25,7 @@ zhfs = set(f for f in zhfs if len(f) > 0)
 # zhfs.pop()
 # print(zhfs)
 zh_fonts = set(f.split(',', 1)[0] for f in outputsplit if len(f.split(',',
-    1)[0] )>0)
+                                                                      1)[0]) > 0)
 # print(zh_fonts)
 available = mat_fonts & zhfs    # matplotlib字体和操作系统字体取交集
 
