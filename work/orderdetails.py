0a605277c1427e3c0465774a733453bb4e532e67
heart5-asus <baiyefeng@gmail.com>
2018-12-27 00:18:59 +0800

    优化订单明细处理模块。模块独立化之后才发现这个板块被淹没了，呜呜

--- work/orderdetails.py
+++ work/orderdetails.py
@@ -15,7 +15,7 @@ import os
 import pandas as pd
 import numpy as np
 import sqlite3 as lite
-import evernote.edam.type.ttypes as Ttypes
+# import evernote.edam.type.ttypes as Ttypes
 import xlrd
 from threading import Timer
 from pathlib import Path
@@ -24,13 +24,14 @@ import pathmagic
 
 with pathmagic.context():
     from func.configpr import cfp, cfpzysm, inizysmpath
-    from func.evernt import get_notestore, imglist2note, tablehtml2evernote, evernoteapijiayi
+    # from func.evernt import get_notestore, imglist2note, tablehtml2evernote, evernoteapijiayi
     from func.logme import log
     from func.first import dirmainpath, dbpathquandan, dbpathdingdanmingxi
-    from func.pdtools import dftotal2top, dataokay
+    # from func.pdtools import dftotal2top, dataokay
+    from func.pdtools import dataokay
 
 
-def chulixls_order(orderfile: Path):
+def chulixls_orderdetails(orderfile: Path):
     try:
         content = xlrd.open_workbook(filename=orderfile, encoding_override='gb18030')
         df = pd.read_excel(content, index_col=0, parse_dates=True, engine='xlrd')
@@ -86,7 +87,8 @@ def chulixls_order(orderfile: Path):
         return
 
 
-def chulidataindir_order(pathorder: Path):
+def chulidataindir_orderdetails(pathorder: Path):
+    notestr = '订单明细'
     cnxp = lite.connect(dbpathdingdanmingxi)
     tablename_order = 'orderdetails'
     sqlstr = "select count(*)  from sqlite_master where type='table' and name = '%s'" % tablename_order
@@ -94,32 +96,31 @@ def chulidataindir_order(pathorder: Path):
     if tablexists:
         # dfresult = pd.DataFrame()
         dfresult = pd.read_sql('select * from \'%s\'' % tablename_order, cnxp, parse_dates=['日期'])
-        log.info('订单数据表%s已存在， 从中读取%d条数据记录。' % (tablename_order, dfresult.shape[0]))
+        log.info('%s数据表%s已存在， 从中读取%d条数据记录。' % (notestr, tablename_order, dfresult.shape[0]))
     else:
-        log.info('订单数据表%s不存在，将创建之。' % tablename_order)
+        log.info('%s数据表%s不存在，将创建之。' % (notestr, tablename_order))
         dfresult = pd.DataFrame()
 
-    notestr = '订单明细'
     if cfpzysm.has_section(notestr) is False:
         cfpzysm.add_section(notestr)
         cfpzysm.write(open(inizysmpath, 'w', encoding='utf-8'))
     files = os.listdir(str(pathorder))
     for fname in files:
-        if fname.startswith('订单明细') and (fname.endswith('xls') or fname.endswith('xlsx')):
+        if fname.startswith(notestr) and (fname.endswith('xls') or fname.endswith('xlsx')):
             yichulifilelist = list()
-            if cfpzysm.has_option('订单明细', '已处理文件清单'):
-                yichulifilelist = cfpzysm.get('订单明细', '已处理文件清单').split()
+            if cfpzysm.has_option(notestr, '已处理文件清单'):
+                yichulifilelist = cfpzysm.get(notestr, '已处理文件清单').split()
             if fname in yichulifilelist:
                 continue
             print(fname, end='\t')
-            dffname = chulixls_order(pathorder / fname)
+            dffname = chulixls_orderdetails(pathorder / fname)
             if dffname is None:
                 continue
             dfresult = dfresult.append(dffname)
             print(dffname.shape[0], end='\t')
             print(dfresult.shape[0])
             yichulifilelist.append(fname)
-            cfpzysm.set('订单明细', '已处理文件清单', '%s' % '\n'.join(yichulifilelist))
+            cfpzysm.set(notestr, '已处理文件清单', '%s' % '\n'.join(yichulifilelist))
             cfpzysm.write(open(inizysmpath, 'w', encoding='utf-8'))
 
     # dfresult.drop_duplicates(['单据编号', '日期', '订单编号', '客户名称', '业务人员', '订单金额', '部门'], inplace=True)
@@ -130,25 +131,31 @@ def chulidataindir_order(pathorder: Path):
     datezhiyu = max(dfresult['日期'])
     print(f'除重后有{dfresult.shape[0]}条记录；数据起于{dateqiyu}，止于{datezhiyu}')
     dfttt = dfresult.drop_duplicates()
-    if cfpzysm.has_option('订单明细', '记录数'):
-        jilucont = cfpzysm.getint('订单明细', '记录数')
+    if cfpzysm.has_option(notestr, '记录数'):
+        jilucont = cfpzysm.getint(notestr, '记录数')
     else:
         jilucont = 0
     if dfttt.shape[0] > jilucont:
         dfttt.to_sql(tablename_order, cnxp, index=False, if_exists='replace')
-        cfpzysm.set('订单明细', '记录数', '%d' % dfttt.shape[0])
+        cnxp.close()
+        cfpzysm.set(notestr, '记录数', '%d' % dfttt.shape[0])
         cfpzysm.write(open(inizysmpath, 'w', encoding='utf-8'))
-        log.info('增加有效订单明细数据%d条。' % (dfttt.shape[0] - jilucont))
-
-    cnxp.close()
+        log.info('增加有效%s数据%d条。' % (notestr, dfttt.shape[0] - jilucont))
+    else:
+        cnxp.close()
+        log.info(f'{notestr}数据无新增数据')
+        return False
 
     return dfttt
 
 
-def jiaoyanchanpinkehu():
+def orderdetails_check4product_customer():
     targetpath = dirmainpath / 'data' / 'work' / '订单明细'
     # chulixls_order(targetpath / '订单明细20180614.xls.xls')
-    dforder = chulidataindir_order(targetpath)
+    dforder = chulidataindir_orderdetails(targetpath)
+    if not dforder:
+        log.info(f'订单明细数据无新增数据，本次产品和客户校验跳过。')
+        return
 
     cnxp = lite.connect(dbpathquandan)
     dataokay(cnxp)
@@ -196,129 +203,11 @@ def jiaoyanchanpinkehu():
     cnxp.close()
 
 
-def showorderstat():
-    # xlsfile = 'data\\work\\销售订单\\销售订单20180606__20180607034848_480667.xls'
-    # dforder = chulixls_order(xlsfile)
-    pathor = dirmainpath / 'data' / 'work' / '销售订单'
-    dforder = chulidataindir_order(pathor)
-    jiaoyanchanpinkehu()
-    dforder = dforder.loc[:, ['日期', '订单编号', '区域', '类型', '客户名称', '业务人员', '订单金额']]
-    dforder.sort_values(by=['日期', '订单编号', '业务人员'], ascending=False, inplace=True)
-    zuixinriqi = dforder.groupby(['日期'])['日期'].size().index.max()
-    orderdatestr = zuixinriqi.strftime('%F')
-    print(orderdatestr, end='\t')
-    dforderzuixinriqi = dforder[dforder.日期 == zuixinriqi]
-    print(dforderzuixinriqi.shape[0])
-    persons = list(dforderzuixinriqi.groupby('业务人员')['业务人员'].count().index)
-    # print(persons)
-    notestr = '每日销售订单核对'
-    if cfpzysm.has_section(notestr) is False:
-        cfpzysm.add_section(notestr)
-        cfpzysm.write(open(inizysmpath, 'w', encoding='utf-8'))
-    for person in persons:
-        if cfpzysm.has_option(notestr + 'guid', person) is False:
-            try:
-                notestore = get_notestore()
-                plannote = Ttypes.Note()
-                plannote.title = notestr + person
-                nbody = '<?xml version="1.0" encoding="UTF-8"?>'
-                nbody += '<!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd">'
-                nbody += '<en-note>%s</en-note>' % plannote.title
-                plannote.content = nbody
-                global workplannotebookguid
-                plannote.notebookGuid = workplannotebookguid
-                # global cfp
-                token = cfp.get('evernote', 'token')
-                note = notestore.createNote(token, plannote)
-                evernoteapijiayi()
-                cfpzysm.set(notestr + 'guid', person, '%s' % note.guid)
-                cfpzysm.write(open(inizysmpath, 'w', encoding='utf-8'))
-                log.info('成功创建%s的%s笔记' % (person, notestr))
-            except Exception as ee:
-                log.critical('创建%s的%s笔记时出现错误。%s' % (person, notestr, str(ee)))
-                continue
-        if cfpzysm.has_option(notestr + 'guid', person + '最新订单日期'):
-            ordertoday = cfpzysm.get(notestr + 'guid', person + '最新订单日期')
-            if zuixinriqi <= pd.to_datetime(ordertoday):  # and False:
-                continue
-        dfperson = dforderzuixinriqi[dforderzuixinriqi.业务人员 == person]
-        dfpersonsum = dfperson.groupby('业务人员').sum()['订单金额']
-        del dfperson['业务人员']
-        del dfperson['日期']
-        print(person, end='\t')
-        print(dfpersonsum[0], end='\t')
-        personguid = cfpzysm.get(notestr + 'guid', person)
-        print(personguid)
-        neirong = tablehtml2evernote(dftotal2top(dfperson), f'{orderdatestr}{notestr}——{person}', withindex=False)
-        # print(neirong)
-        try:
-            notestore = get_notestore()
-            imglist2note(notestore, [], personguid, '%s——%s（%s）' % (notestr, person, orderdatestr), neirong)
-            cfpzysm.set(notestr + 'guid', person + '最新订单日期', '%s' % orderdatestr)
-            cfpzysm.write(open(inizysmpath, 'w', encoding='utf-8'))
-        except Exception as eeee:
-            log.critical('更新笔记%s——%s（%s）时出现严重错误。%s' % (notestr, person, orderdatestr, str(eeee)))
-    else:
-        log.info('下列人员的销售订单正常处置完毕：%s' % persons)
-
-    yuechuriqi = pd.to_datetime(f"{zuixinriqi.strftime('%Y')}-{zuixinriqi.strftime('%m')}-01")
-    dfsales = pd.DataFrame(dforder[dforder.日期 >= yuechuriqi])
-    dfsales = dfsales.groupby(['区域', '类型', '客户名称', '业务人员'], as_index=False).sum()
-    dfsales.sort_values(['区域', '订单金额'], inplace=True)
-    notestr = '销售订单金额（月）'
-    if cfpzysm.has_section(notestr) is False:
-        cfpzysm.add_section(notestr)
-        cfpzysm.write(open(inizysmpath, 'w', encoding='utf-8'))
-    for person in persons:
-        if cfpzysm.has_option(notestr, person) is False:
-            try:
-                notestore = get_notestore()
-                plannote = Ttypes.Note()
-                plannote.title = notestr + person
-                nbody = '<?xml version="1.0" encoding="UTF-8"?>'
-                nbody += '<!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd">'
-                nbody += '<en-note>%s</en-note>' % plannote.title
-                plannote.content = nbody
-                plannote.notebookGuid = workplannotebookguid
-                # cfp, cfppath = getcfp('everwork')
-                token = cfp.get('evernote', 'token')
-                note = notestore.createNote(token, plannote)
-                evernoteapijiayi()
-                cfpzysm.set(notestr, person, '%s' % note.guid)
-                cfpzysm.write(open(inizysmpath, 'w', encoding='utf-8'))
-                log.info('成功创建%s的%s笔记' % (person, notestr))
-            except Exception as ee:
-                log.critical('创建%s的%s笔记时出现错误。%s' % (person, notestr, str(ee)))
-                continue
-        if cfpzysm.has_option(notestr, person + '最新订单日期'):
-            ordertoday = cfpzysm.get(notestr, person + '最新订单日期')
-            if zuixinriqi <= pd.to_datetime(ordertoday):  # and False:
-                continue
-        dfperson = dfsales[dfsales.业务人员 == person]
-        dfpersonsum = dfperson['订单金额'].sum()
-        del dfperson['业务人员']
-        print(person, end='\t')
-        print(dfpersonsum, end='\t')
-        personguid = cfpzysm.get(notestr, person)
-        print(personguid)
-        neirong = tablehtml2evernote(dftotal2top(dfperson), f'{orderdatestr[:-3]}{notestr}', withindex=False)
-        # print(neirong)
-        try:
-            notestore = get_notestore()
-            imglist2note(notestore, [], personguid, '%s——%s（%s）' % (notestr, person, orderdatestr[:-3]), neirong)
-            cfpzysm.set(notestr, person + '最新订单日期', '%s' % orderdatestr)
-            cfpzysm.write(open(inizysmpath, 'w', encoding='utf-8'))
-        except Exception as eeee:
-            log.critical('更新笔记%s——%s（%s）时出现严重错误。%s' % (notestr, person, orderdatestr, str(eeee)))
-    else:
-        log.info('下列人员的销售订单金额月度分析正常处置完毕：%s' % persons)
-
-
 def showorderstat2note(jiangemiao):
     global workplannotebookguid
     workplannotebookguid = '2c8e97b5-421f-461c-8e35-0f0b1a33e91c'
     try:
-        showorderstat()
+        orderdetails_check4product_customer()
     except Exception as ee:
         log.critical('处理订单核对统计笔记时出现错误。%s' % str(ee))
         raise ee
@@ -330,6 +219,6 @@ def showorderstat2note(jiangemiao):
 
 if __name__ == '__main__':
     log.info(f'测试文件\t{__file__}')
-    jiaoyanchanpinkehu()
-    showorderstat2note(60 * 15)
+    orderdetails_check4product_customer()
+    # showorderstat2note(60 * 15)
     print('Done.测试完毕。')
