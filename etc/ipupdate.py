15353a28ccad8da13752456d8e236e4cd262dd37
heart5 <baiyefeng@gmail.com>
2019-01-11 20:33:08 +0800

    修正了登记新纪录的逻辑，ip或ssid不同就登记，之前的and关系是错误的

--- etc/ipupdate.py
+++ etc/ipupdate.py
@@ -96,7 +96,7 @@ def showiprecords():
         cfp.set(device_id, 'start', start)
         cfp.write(open(cfppath, 'w', encoding='utf-8'))
 
-    if (ip != ipr) and (wifiid != wifiidr):
+    if (ip != ipr) or (wifiid != wifiidr):
         txtfilename = str(dirmainpath / 'data' / 'ifttt' /
                           f'ip_{device_id}.txt')
         print(txtfilename)
