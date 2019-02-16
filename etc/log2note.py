b18679a1be2950dc974dd4f957dca6183e6e15ca
heart5 <baiyefeng@gmail.com>
2019-02-17 01:32:25 +0800

    日志笔记更新实现不同服务器分别显示在不同的笔记中

--- etc/log2note.py
+++ etc/log2note.py
@@ -14,16 +14,17 @@ import pandas as pd
 with pathmagic.context():
     from func.first import getdirmain
     from func.configpr import getcfp
-    from func.evernttest import get_notestore, imglist2note, readinifromnote
+    from func.evernttest import get_notestore, imglist2note, readinifromnote, token, evernoteapijiayi, makenote
     from func.logme import log
     from func.wrapfuncs import timethis, ift2phone
-    from func.termuxtools import termux_location
+    from func.termuxtools import termux_location, termux_telephony_deviceinfo
     from func.nettools import ifttt_notify
+    import evernote.edam.type.ttypes as ttypes
 
 
 @timethis
 # @ift2phone()
-def log2note(noteguid, loglimit, levelstr=''):
+def log2note(noteguid, loglimit, levelstr='', notetitle='everwork日志信息'):
     namestr = 'everlog'
     cfp, cfppath = getcfp(namestr)
     if not cfp.has_section(namestr):
@@ -83,7 +84,7 @@ def log2note(noteguid, loglimit, levelstr=''):
         try:
             nstore = get_notestore()
             imglist2note(nstore, [], noteguid,
-                         f'everwork{levelstr4title}日志信息', loglinestr)
+                         notetitle, loglinestr)
             cfp.set(namestr, countnameinini, f'{len(loglines)}')
             cfp.write(open(cfppath, 'w', encoding='utf-8'))
             print(f'新的log{levelstr4title}信息成功更新入笔记')
@@ -92,9 +93,51 @@ def log2note(noteguid, loglimit, levelstr=''):
             log.critical(errmsg)
             ifttt_notify(errmsg, 'log2note')
 
+def log2notes():
+    namestr = 'everlog'
+    cfplog, cfplogpath = getcfp(namestr)
+    if not cfplog.has_section(namestr):
+        cfplog.add_section(namestr)
+        cfplog.write(open(cfplogpath, 'w', encoding='utf-8'))
+    if cfplog.has_option(namestr, 'device_id'):
+        device_id = cfplog.get(namestr, 'device_id')
+    else:
+        outputdict = termux_telephony_deviceinfo()
+        device_id = outputdict["device_id"].strip()
+        cfplog.set(namestr, 'device_id', device_id)
+        cfplog.write(open(cfplogpath, 'w', encoding='utf-8'))
+        log.info(f'获取device_id:\t{device_id}，并写入ini文件：\t{cfplogpath}')
+    if not cfplog.has_section(device_id):
+        cfplog.add_section(device_id)
+        cfplog.write(open(cfplogpath, 'w', encoding='utf-8'))
 
-if __name__ == '__main__':
-    print(f'开始运行文件\t{__file__}')
+    global token
+    if cfplog.has_option(device_id, 'logguid'):
+        logguid = cfplog.get(device_id, 'logguid')
+    else:
+        note_store = get_notestore()
+        parentnotebook = note_store.getNotebook('4524187f-c131-4d7d-b6cc-a1af20474a7f')
+        evernoteapijiayi()
+        note = ttypes.Note()
+        note.title = f'服务器_{device_id}_日志信息'
+        notelog = makenote(token, note_store, note.title, notebody='', parentnotebook=parentnotebook)
+        logguid = notelog.guid
+        cfplog.set(device_id, 'logguid', logguid)
+        cfplog.write(open(cfplogpath, 'w', encoding='utf-8'))
+
+    if cfplog.has_option(device_id, 'logcguid'):
+        logcguid = cfplog.get(device_id, 'logcguid')
+    else:
+        note_store = get_notestore()
+        parentnotebook = note_store.getNotebook('4524187f-c131-4d7d-b6cc-a1af20474a7f')
+        evernoteapijiayi()
+        note = ttypes.Note()
+        note.title = f'服务器_{device_id}_严重错误日志信息'
+        notelog = makenote(token, note_store, note.title, notebody='', parentnotebook=parentnotebook)
+        logcguid = notelog.guid
+        cfplog.set(device_id, 'logcguid', logcguid)
+        cfplog.write(open(cfplogpath, 'w', encoding='utf-8'))
+
     readinifromnote()
     cfpfromnote, cfpfromnotepath = getcfp('everinifromnote')
     namestr = 'everlog'
@@ -102,17 +145,26 @@ if __name__ == '__main__':
         loglimitc = cfpfromnote.getint(namestr, 'loglimit')
     else:
         loglimitc = 500
+    if cfpfromnote.has_option('device', device_id):
+        servername = cfpfromnote.get('device', device_id)
+    else:
+        servername = device_id
 
     cfpeverwork, cfpeverworkpath = getcfp('everwork')
 
     if cfpfromnote.has_option(namestr, 'critical') and (cfpfromnote.getboolean(namestr, 'critical') == True):
         levelstrc = 'CRITICAL'
-        noteguidc = cfpeverwork.get('evernote', 'lognotecriticalguid')
-        log2note(noteguidc, loglimitc, levelstrc)
+        # noteguidc = cfpeverwork.get('evernote', 'lognotecriticalguid')
+        log2note(logcguid, loglimitc, levelstrc, notetitle=f'服务器_{servername}_严重错误日志信息')
 
-    noteguidn = cfpeverwork.get('evernote', 'lognoteguid')
-    log2note(noteguid=noteguidn, loglimit=loglimitc)
+    # noteguidn = cfpeverwork.get('evernote', 'lognoteguid')
+    log2note(noteguid=logguid, loglimit=loglimitc, notetitle=f'服务器_{servername}_日志信息')
 
     # locinfo = termux_location()
     # print(locinfo)
+ 
+
+if __name__ == '__main__':
+    print(f'开始运行文件\t{__file__}')
+    log2notes()
     print(f'Done.结束执行文件\t{__file__}')
