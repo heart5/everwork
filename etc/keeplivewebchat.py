c712d0d881652f5868120eea29c02f4ba4a88357
heart5 <baiyefeng@gmail.com>
2019-05-15 23:17:00 +0800

    保持webchat常驻

---
+++ etc/keeplivewebchat.py
@@ -0,0 +1,32 @@
+"""
+让微信常驻运行，并保留有效pkg文件以便下次热启动而不用重新扫码
+"""
+
+import os
+import shutil
+
+import pathmagic
+with pathmagic.context():
+    from func.first import dirmainpath, touchfilepath2depth
+    from func.logme import log
+    from etc.guanliprocess import judgeprocess
+    
+
+def keeplivewebchat():
+    srcdir = dirmainpath
+    trtdir = dirmainpath / 'data' / 'webchat'
+    touchfilepath2depth(trtdir)
+    fname = 'itchat.pkl'
+    if judgeprocess("python life/webchat.py"):
+        print(f"微信程序正常运行")
+        shutil.copy(srcdir / fname, trtdir / fname)
+    else:
+        shutil.copy(trtdir / fname, srcdir / fname)
+        stdout = os.popen(f"cd {dirmainpath};python life/webchat.py")
+        print(f"{stdout.read()}")
+
+
+if __name__ == '__main__':
+    log.info(f'运行文件\t{__file__}')
+    keeplivewebchat()
+    log.info(f"文件\t{__file__}\t运行结束。")
