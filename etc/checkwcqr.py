f08ecf24286832bea0080fb226bc7e6448ccb244
heart5 <baiyefeng@gmail.com>
2019-06-08 14:36:21 +0800

    增加对qr是否更新的判断，决定是否邮件至邮箱和笔记

---
+++ etc/checkwcqr.py
@@ -0,0 +1,42 @@
+"""
+检查是否有新的QR，有就发送邮件
+"""
+
+import os
+import time
+import pathmagic
+
+with pathmagic.context():
+    from func.logme import log
+    from func.first import getdirmain
+    from etc.mailfun import mailfileindir
+    from func.configpr import getcfpoptionvalue, setcfpoptionvalue
+
+
+def findnewqrthensendmail():
+    fl = 'QR.png'
+    qrfile = getdirmain() / fl
+    if os.path.exists(qrfile):
+        print(qrfile)
+        print(os.path.abspath(qrfile))
+        qrfiletimeini = getcfpoptionvalue('everwebchat', 'webchat', 'qrfiletime') 
+        qrfiletimenew = str(os.stat(qrfile).st_mtime)
+        print(f"{qrfiletimeini}\t{qrfiletimenew}")
+        if qrfiletimeini:
+            if qrfiletimenew > qrfiletimeini:
+                mailfileindir(getdirmain(), fl)
+                setcfpoptionvalue('everwebchat', 'webchat', 'qrfiletime',
+                                  qrfiletimenew)
+            print(qrfiletimeini)
+        else:
+            mailfileindir(getdirmain(), fl)
+            setcfpoptionvalue('everwebchat', 'webchat',
+                              'qrfiletime',qrfiletimenew)
+    else:
+        print(f"{fl}不存在")
+
+
+if __name__ == '__main__':
+    log.info(f'运行文件\t{__file__}')
+    findnewqrthensendmail()
+    log.info(f"文件\t{__file__}\t运行结束。")
