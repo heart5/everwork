f5fdeee96bd1afee05ab74743fc619d2c6266a5d
heart5 <baiyefeng@gmail.com>
2019-06-19 09:07:57 +0800

    函数主体移入函数库；增加了对cron的检查处理

--- etc/checkwcqr.py
+++ etc/checkwcqr.py
@@ -9,42 +9,25 @@ import pathmagic
 with pathmagic.context():
     from func.logme import log
     from func.first import getdirmain
-    from etc.mailfun import mailfileindir
-    from func.configpr import getcfpoptionvalue, setcfpoptionvalue
-    from etc.getid import getdeviceid
-    from func.evernttest import timestamp2str, imglist2note, note_store, getinivaluefromnote
+    from func.mailsfunc import findnewthenupdatenote
 
 
 def findnewqrthensendmail():
     fl = 'QR.png'
     qrfile = getdirmain() / fl
-    if os.path.exists(qrfile):
-        # print(qrfile)
-        # print(os.path.abspath(qrfile))
-        qrfiletimeini = getcfpoptionvalue('everwebchat', 'webchat', 'qrfiletime') 
-        qrfilesecsnew = os.stat(qrfile).st_mtime
-        qrfiletimenew = str(qrfilesecsnew)
-        if qrfiletimeini:
-            qrftlst = qrfiletimeini.split(',')
-            print(timestamp2str(float(qrftlst[0])))
-            if (qrfiletimenew > qrftlst[0]): # or True:
-                qrtstr = f"{qrfiletimenew},{qrfiletimeini}"
-                qrtstrlst = qrtstr.split(',')
-                qrtstrflst = [timestamp2str(float(x)) for x in qrtstrlst]
-                targetstr = '<pre>'+'\n'.join(qrtstrflst)+'</pre>'
-                qrnoteguid = getinivaluefromnote('webchat', f"qr{getdeviceid()}")
-                imglist2note(note_store, [qrfile], qrnoteguid,
-                             f"{getinivaluefromnote('device', getdeviceid())} QR微信二维码", targetstr)
-                mailfileindir(getdirmain(), fl)
-                setcfpoptionvalue('everwebchat', 'webchat', 'qrfiletime', qrtstr)
-        else:
-            mailfileindir(getdirmain(), fl)
-            setcfpoptionvalue('everwebchat', 'webchat', 'qrfiletime',qrfiletimenew)
-    else:
-        print(f"{fl}不存在")
+    findnewthenupdatenote(qrfile, 'everwebchat', 'webchat', 'qr', 'QR微信二维码')
+    
 
+def findnewcronthenupdate():
+    r = os.popen('whoami')
+    me = r.read()[:-1]
+    cronfile = f'/data/data/com.termux/files/usr/var/spool/cron/crontabs/{me}'
+    findnewthenupdatenote(cronfile, 'everwork', 'everwork', 'cron',
+                          'cron自动运行排期表')
+    
 
 if __name__ == '__main__':
     # log.info(f'运行文件\t{__file__}')
-    findnewqrthensendmail()
+    # findnewqrthensendmail()
+    findnewcronthenupdate()
     # log.info(f"文件\t{__file__}\t运行结束。")
