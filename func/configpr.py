e4d05756fae552aeb6a3b58367a8e0eff329902c
heart5 <baiyefeng@gmail.com>
2019-07-07 15:17:27 +0800

    优化optionvalue输出，自动识别布尔值、整数和浮点数

--- func/configpr.py
+++ func/configpr.py
@@ -3,6 +3,7 @@
 输出相应的配置处理器
 可用的处理有：everwork, everdatanote, everlife, everzysm, everworkplan
 """
+import re
 from pathlib import Path
 from configparser import ConfigParser
 import pathmagic
@@ -32,13 +33,40 @@ def setcfpoptionvalue(cfpfilename: str, sectionname: str, optionname: str, optio
 def getcfpoptionvalue(cfpfilename: str, sectionname: str, optionname: str):
     cfpin, cfpinpath = getcfp(cfpfilename)
     if not cfpin.has_section(sectionname):
-        # cfpin.add_section(sectionname)
-        # cfpin.write(open(cfpinpath, 'w', encoding='utf-8'))
+        print(f"seticon {sectionname} is not exsits.")
         return
     if not cfpin.has_option(sectionname, optionname):
+        print(f"option {optionname} is not exsits.")
         return
 
-    return cfpin.get(sectionname, optionname)
+    targetvalue = cfpin.get(sectionname, optionname)
+
+    # 处理布尔值
+    if targetvalue.strip().lower() == 'true':
+        return True
+
+    if targetvalue.strip().lower() == 'false':
+        return False
+
+    # 处理整数
+    ptn = re.compile(r"^[+-]?[0-9]+$")
+    result = ptn.match(targetvalue)
+    if result:
+        targetvalue = int(result.group())
+        return targetvalue
+    
+    # 处理小数
+    ptn = re.compile(r"^[+-]?[0-9]+\.[0-9]+$")
+    result = ptn.match(targetvalue)
+    if result:
+        targetvalue = float(result.group())
+        return targetvalue
+    # if isinstance(targetvalue, int):
+        # targetvalue = int(targetvalue)
+    # elif isinstance(targetvalue, float):
+        # targetvalue = float(targetvalue)
+
+    return targetvalue
 
 
 cfp, inifilepath = getcfp('everwork')
