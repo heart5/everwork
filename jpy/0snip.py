b48d0be956e729d480bb81c5e5a0877977179ce0
heart5 <baiyefeng@gmail.com>
2019-10-11 12:41:55 +0800

    测试用的小器件

---
+++ jpy/0snip.py
@@ -0,0 +1,20 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.4'
+#       jupytext_version: 1.2.1
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+race = lambda x: x**0.5
+lop = race(23)
+print(lop)
+
+
