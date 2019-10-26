a1f13780cb08f9228c1440f1c626000a78751c49
heart5 <baiyefeng@gmail.com>
2019-10-27 00:41:00 +0800

    测试mate7下的jupyter

--- termuxtest.py
+++ termuxtest.py
@@ -1,22 +1,17 @@
 # -*- coding: utf-8 -*-
 # ---
 # jupyter:
-#   jupytext_format_version: '1.3'
-#   jupytext_formats: py:light
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.4'
+#       jupytext_version: 1.2.1
 #   kernelspec:
 #     display_name: Python 3
 #     language: python
 #     name: python3
-#   language_info:
-#     codemirror_mode:
-#       name: ipython
-#       version: 3
-#     file_extension: .py
-#     mimetype: text/x-python
-#     name: python
-#     nbconvert_exporter: python
-#     pygments_lexer: ipython3
-#     version: 3.6.6
 # ---
 
 import pathmagic
