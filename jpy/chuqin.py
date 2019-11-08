6f644e2999e1d658d000e985cb7ebb498274e084
heart5-asus <baiyefeng@gmail.com>
2019-11-08 17:08:57 +0800

    pycharm添加了辅助注释

--- jpy/chuqin.py
+++ jpy/chuqin.py
@@ -14,51 +14,45 @@
 #     name: python3
 # ---
 
+# + {"pycharm": {"is_executing": false}}
 import os
 import re
-import math
 import pandas as pd
-# import numpy as np
-# from pandas import DataFrame
 
-# +
+# + {"pycharm": {"is_executing": false}}
 import pathmagic
-
 with pathmagic.context():
     from func.first import dirmainpath
     from func.logme import log
-    # from func.configpr import getcfp, getcfpoptionvalue, setcfpoptionvalue
-    from func.configpr import getcfpoptionvalue
-    # from func.wrapfuncs import timethis, ift2phone
+    from func.configpr import getcfp, getcfpoptionvalue, setcfpoptionvalue
+    from func.wrapfuncs import timethis, ift2phone
     from func.evernttest import getinivaluefromnote
 
 
-# -
-
+# + {"pycharm": {"is_executing": false}}
 def chuqinjiluzhengli():
     """
     整理原始出勤记录，输出有用信息
     :return: 有用信息列表
     """
     print(dirmainpath)
-    qdpath = dirmainpath / 'data' / 'work' / '考勤记录'
+    qdpath = dirmainpath / 'data' /'work' / '考勤记录'
     print(qdpath)
-    ptn = re.compile('^员工出勤记录(20\\d{4}).*\\.xls$')
+    ptn = re.compile('^员工出勤记录(20\d{4}).*\.xls$')
     stopcount = 0
     fls = os.listdir(qdpath)
-    print(len(fls), fls)
+    print(len(fls))
     targetlst = []
     for fl in fls:
         timestamp = re.findall(ptn, fl)
-        print(timestamp)
         if timestamp:
             cld = []
             qddf = pd.read_excel(qdpath / fl, sheet_name='考勤记录')
-            shiduan = qddf.iloc[1, 2]  # 时间段
+            shiduan = qddf.iloc[1, 2]   #时间段
             """
             出勤统计时间段
             """
-            zhibiao = qddf.iloc[1, 11]  # 制表时间
+            zhibiao = qddf.iloc[1, 11]  #制表时间
             jiludf = qddf.iloc[3:]
             cld.append(shiduan)
             cld.append(zhibiao)
@@ -70,7 +64,7 @@ def chuqinjiluzhengli():
             jilulst = []
             danganlst = []
             for i in range(renshu):
-                dangan = list()
+                dangan = []
                 dangan.append(jiludf.iloc[i * 2, 2])
                 dangan.append(jiludf.iloc[i * 2, 10])
                 dangan.append(jiludf.iloc[i * 2, 20])
@@ -96,121 +90,35 @@ def chuqinjiluzhengli():
     return targetlst
 
 
+# + {"pycharm": {"is_executing": false}}
 def tongjichuqin():
-    print('56')
     pass
 
 
-def splitjs(jsstr: str):
-    """
-    处理《计时工》原始打卡记录，规范成合规的时间间隔
-    :param jsstr: str
-    :return: str
-    """
-    zhongwuxiaban = getcfpoptionvalue('everinifromnote', 'xingzheng', 'zhongwuxiaban')
-    xiawushangban = getcfpoptionvalue('everinifromnote', 'xingzheng', 'xiawushangban')
-    xiawuxiaban = getcfpoptionvalue('everinifromnote', 'xingzheng', 'xiawuxiaban')
-    ptn = re.compile('\\d{2}:\\d{2}')
-    findlst = re.findall(ptn, jsstr)
-    if len(findlst) == 2:
-        if (findlst[0] <= zhongwuxiaban) & (findlst[1] >= xiawushangban):
-            findlst.insert(1, xiawushangban)
-            findlst.insert(1, zhongwuxiaban)
-            log.info(f"计时工打卡记录条目({len(findlst)})增补中午休息间隔\t{findlst}")
-        elif (findlst[0] >= zhongwuxiaban) & (findlst[1] >= xiawushangban):
-            log.info(f"计时工打卡记录条目({len(findlst)})属于正常下午班")
-        elif (findlst[0] <= zhongwuxiaban) & (findlst[1] <= xiawushangban):
-            log.info(f"计时工打卡记录条目({len(findlst)})属于正常上午班")
-        else:
-            log.critical(f"出现不合规范的计时工打卡记录({len(findlst)})\t{findlst}")
-            findlst = None
-    elif len(findlst) == 3:
-        if findlst[0] <= zhongwuxiaban:
-            findlst.insert(1, zhongwuxiaban)
-            log.info(f"计时工打卡记录条目({len(findlst)})增补中午休息间隔\t{findlst}")
-        else:
-            log.critical(f"出现不合规范的计时工打卡记录({len(findlst)})\t{findlst}")
-            findlst = None
-    elif len(findlst) == 1:
-        if findlst[0] <= zhongwuxiaban:
-            findlst.append(zhongwuxiaban)
-            log.info(f"计时工打卡记录条目({len(findlst)})增补中午休止\t{findlst}")
-        elif findlst[0] >= zhongwuxiaban:
-            findlst.append(xiawuxiaban)
-            log.info(f"计时工打卡记录条目({len(findlst)})增补下午休止\t{findlst}")
-        else:
-            log.critical(f"出现不合规范的计时工打卡记录({len(findlst)})\t{findlst}")
-            findlst = None
-    elif len(findlst) == 4:
-        pass
-    elif len(findlst) != 0:
-        log.critical(f"出现不合规范的计时工打卡记录({len(findlst)})\t{findlst}")
-        findlst = None
-    resultlst = findlst
-
-    return resultlst
+# + {"pycharm": {"is_executing": false}}
+def tongjichuqinjishigong():
+    pass
 
 
-def computejishi(inputlst: list):
-    """
-    计算工时
-    :param inputlst:
-    :return:
-    """
-    if (len(inputlst) % 2) != 0:
-        log.critical(f"打卡记录非偶数\t{inputlst}")
-        return
-
-    # print(inputlst)
-    totalminute: int = 0
-    for i in range(0, len(inputlst), 2):
-        frtlst = inputlst[i].split(":")
-        print(frtlst, end='\t')
-        frtmin = int(frtlst[0]) * 60
-        frtmin += int(frtlst[1])
-        seclst = inputlst[i+1].split(":")
-        print(seclst)
-        secmin = int(seclst[0]) * 60
-        secmin += int(seclst[1])
-        totalminute += secmin - frtmin
-
-    return totalminute
-
-
-def tongjichuqinjishigong(inputs: pd.Series):
-    """
-    统计工时
-    :param inputs: 打卡，Series
-    :return: 出勤天数，分钟数累计，工时数
-    """
-    df = pd.DataFrame(inputs)
-    df.columns = ['打卡记录']
-    # print(df)
-    df['整理'] = df[df.columns[0]].apply(lambda x: splitjs(str(x)) if x else None)
-    df['分钟'] = df[df.columns[1]].apply(lambda x: computejishi(x) if x else None)
-
-    return df['分钟'].count(), df['分钟'].sum(), math.ceil(df['分钟'].sum() / 60)
-
-
+# + {"pycharm": {"is_executing": false}}
 def tongjichuqinixingzheng():
     pass
 
 
+# + {"pycharm": {"is_executing": false}}
 qdlst = chuqinjiluzhengli()
-print(qdlst)
-jilusdf = qdlst[-1][4]
+jiludf = qdlst[-1][4]
 ptnsep = re.compile('[,，]')
 jslst = re.split(ptnsep, getinivaluefromnote('xingzheng', '计时工'))
 xzlst = re.split(ptnsep, getinivaluefromnote('xingzheng', '行政岗位'))
-for name in jilusdf.columns:
+for name in jiludf.columns:
     if name in jslst:
-        chugongtianshu: int
-        fenzhongleiji: int
-        gongshi: int
-        chugongtianshu, fenzhongleiji, gongshi = tongjichuqinjishigong(jilusdf[name])
-        print(f"计时工\t{name}\t{chugongtianshu}\t{fenzhongleiji}\t{gongshi}")
+        print(f"计时工\t{name}")
     elif name in xzlst:
         print(f"行政岗位\t{name}")
     else:
         print(f"正常岗位\t{name}")
 # print(qdlst)
+
+# + {"pycharm": {"is_executing": false}}
+
