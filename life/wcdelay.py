ec18da63904b8ff6ef6f5ade69ef38321eb131e5
heart5 <baiyefeng@gmail.com>
2020-03-02 01:23:45 +0800

    调用litetools库中的函数；代码格式化

--- life/wcdelay.py
+++ life/wcdelay.py
@@ -6,55 +6,23 @@ import os
 import sqlite3 as lite
 import pandas as pd
 import matplotlib.pyplot as plt
-from pandas.plotting import register_matplotlib_converters
-# import datetime
 import time
 import datetime
+from pandas.plotting import register_matplotlib_converters
 
 import pathmagic
+
 with pathmagic.context():
     from func.logme import log
     from func.first import touchfilepath2depth, getdirmain
-
-
-def ifnotcreate(tablen: str, dbn: str):
-    """
-    如果没有相应数据表就创建一个
-    :param tablen:
-    :param dbn:
-    :return:
-    """
-    conn = lite.connect(dbn)
-
-    try:
-
-        cursor = conn.cursor()
-
-        def istableindb(tablenin: str):
-            cursor.execute("select * from sqlite_master where type='table'")
-            table = cursor.fetchall()
-            # print(table)
-            chali = [x for item in table for x in item[1:3]]
-            # print(chali)
-
-            return tablenin in chali
-
-        if not istableindb(tablen):
-            createsql = f'create table {tablen} (time int primary key, delay int)'
-            cursor.execute(createsql)
-            conn.commit()
-            log.info(f"数据表：\t{tablen} 被创建成功。\t{createsql}")
-
-    except Exception as eee:
-        log.critical(f"操作数据库时出现错误。{dbn}\t{eee}")
-    finally:
-        conn.close()
+    from func.litetools import ifnotcreate
 
 
 def inserttimeitem2db(timestampinput: int):
-    dbname = touchfilepath2depth(getdirmain() / 'data' / 'db' / 'wcdelay.db')
-    tablename = 'wcdelay'
-    ifnotcreate(tablename, dbname)
+    dbname = touchfilepath2depth(getdirmain() / "data" / "db" / "wcdelay.db")
+    tablename = "wcdelay"
+    csql = f"create table {tablename} (time int primary key, delay int)"
+    ifnotcreate(tablename, csql, dbname)
 
     # timetup = time.strptime(timestr, "%Y-%m-%d %H:%M:%S")
     # timest = time.mktime(timetup)
@@ -62,7 +30,9 @@ def inserttimeitem2db(timestampinput: int):
     conn = lite.connect(dbname)
     try:
         cursor = conn.cursor()
-        cursor.execute(f"insert into {tablename} values(?, ?)", (timestampinput, elsmin))
+        cursor.execute(
+            f"insert into {tablename} values(?, ?)", (timestampinput, elsmin)
+        )
         print(f"数据成功写入{dbname}\t{(timestampinput, elsmin)}")
         conn.commit()
     except lite.IntegrityError as lie:
@@ -72,10 +42,11 @@ def inserttimeitem2db(timestampinput: int):
 
 
 def getdelaydb():
-    dbname = touchfilepath2depth(getdirmain() / 'data' / 'db' / 'wcdelay.db')
-    tablename = 'wcdelay'
+    dbname = touchfilepath2depth(getdirmain() / "data" / "db" / "wcdelay.db")
+    tablename = "wcdelay"
 
-    ifnotcreate(tablename, dbname)
+    csql = f"create table {tablename} (time int primary key, delay int)"
+    ifnotcreate(tablename, csql, dbname)
 
     conn = lite.connect(dbname)
     cursor = conn.cursor()
@@ -83,17 +54,20 @@ def getdelaydb():
     table = cursor.fetchall()
     conn.close()
 
-    timedf = pd.DataFrame(table, columns=['time', 'delay'])
-    timedf['time'] = timedf['time'].apply(
-        lambda x: pd.to_datetime(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(x))))
-    timedf.set_index('time', inplace=True)
+    timedf = pd.DataFrame(table, columns=["time", "delay"])
+    timedf["time"] = timedf["time"].apply(
+        lambda x: pd.to_datetime(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(x)))
+    )
+    timedf.set_index("time", inplace=True)
 
     if (tdfsize := timedf.shape[0]) != 0:
         print(f"延时记录共有{tdfsize}条")
         # 增加当前时间，延时值引用最近一次的值，用于做图形展示的右边栏
-#         nowtimestamp = time.ctime()
-#         timedf = timedf.append(pd.DataFrame([timedf.iloc[-1]], index=[pd.to_datetime(time.ctime())]))
-        timedf = timedf.append(pd.DataFrame([timedf.iloc[-1]], index=[pd.to_datetime(time.ctime())]))
+        #         nowtimestamp = time.ctime()
+        #         timedf = timedf.append(pd.DataFrame([timedf.iloc[-1]], index=[pd.to_datetime(time.ctime())]))
+        timedf = timedf.append(
+            pd.DataFrame([timedf.iloc[-1]], index=[pd.to_datetime(time.ctime())])
+        )
         jujinmins = int((timedf.index[-1] - timedf.index[-2]).total_seconds() / 60)
     else:
         jujinmins = 0
@@ -106,15 +80,15 @@ def getdelaydb():
     return jujinmins, timedf
 
 
-def showdelayimg(jingdu: int=300):
+def showdelayimg(jingdu: int = 300):
     jujinm, timedf = getdelaydb()
     print(f"记录新鲜度：出炉了{jujinm}分钟")
-    
+
     register_matplotlib_converters()
 
     plt.figure(figsize=(36, 6))
-    plt.style.use('ggplot')  # 使得作图自带色彩，这样不用费脑筋去考虑配色什么的；
-    
+    plt.style.use("ggplot")  # 使得作图自带色彩，这样不用费脑筋去考虑配色什么的；
+
     # 画出左边界
     tmin = timedf.index.min()
     tmax = timedf.index.max()
@@ -123,16 +97,18 @@ def showdelayimg(jingdu: int=300):
     print(f"左边界：{bianjie}秒，也就是大约{int(bianjie / 60)}分钟")
     # plt.xlim(xmin=tmin-pd.Timedelta(f'{bianjie}s'))
     plt.xlim(xmin=tmin)
-    plt.xlim(xmax=tmax + pd.Timedelta(f'{bianjie}s'))
+    plt.xlim(xmax=tmax + pd.Timedelta(f"{bianjie}s"))
     # plt.vlines(tmin, 0, int(timedf.max() / 2))
     plt.vlines(tmax, 0, int(timedf.max() / 2))
-    
+
     # 绘出主图和标题
     plt.scatter(timedf.index, timedf, s=timedf)
     plt.scatter(timedf[timedf == 0].index, timedf[timedf == 0], s=0.5)
-    plt.title('信息频率和延时')
+    plt.title("信息频率和延时")
 
-    imgwcdelaypath = touchfilepath2depth(getdirmain() / 'img' / 'webchat' / 'wcdelay.png')
+    imgwcdelaypath = touchfilepath2depth(
+        getdirmain() / "img" / "webchat" / "wcdelay.png"
+    )
 
     plt.savefig(imgwcdelaypath, dpi=jingdu)
     print(os.path.relpath(imgwcdelaypath))
@@ -140,9 +116,9 @@ def showdelayimg(jingdu: int=300):
     return imgwcdelaypath
 
 
-if __name__ == '__main__':
-    log.info(f'运行文件\t{__file__}')
+if __name__ == "__main__":
+    log.info(f"运行文件\t{__file__}")
     xinxian, tdf = getdelaydb()
     print(xinxian)
     print(tdf.sort_index(ascending=False))
-    log.info(f'文件{__file__}运行结束')
+    log.info(f"文件{__file__}运行结束")
