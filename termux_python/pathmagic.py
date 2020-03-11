a4d4084aea900dc80bfce86be02bd1bce09deda3
heart5 <baiyefeng@gmail.com>
2020-03-11 12:17:20 +0800

    使termux_python目录可以被引入

---
+++ termux_python/pathmagic.py
@@ -0,0 +1,28 @@
+# encoding:utf-8
+"""
+功能描述
+"""
+
+# import os
+import sys
+
+
+# bp = os.path.dirname(os.path.realpath('.')).split(os.sep)
+# modpath = os.sep.join(bp + ['src'])
+# sys.path.insert(0, modpath)
+# sys.path.insert(0,os.sep.join(bp))
+
+
+class context:
+    def __enter__(self):
+        sys.path.extend(['../../', '..', '.'])
+
+    def __exit__(self, *args):
+        pass
+
+
+if __name__ == '__main__':
+    print(f'运行文件\t{__file__}')
+    for pp in sys.path:
+        print(pp)
+    print('Done.完毕。')
