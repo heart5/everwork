22a635f2eb2e3cae59efc27147eae242a1d86083
heart5 <baiyefeng@gmail.com>
2020-03-11 12:18:41 +0800

    做好import，测试电池命令的运行

--- termux_python/termuxutils.py
+++ termux_python/termuxutils.py
@@ -6,13 +6,20 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.3.1
+#       jupytext_version: 1.3.5
 #   kernelspec:
 #     display_name: Python 3
 #     language: python
 #     name: python3
 # ---
 
-# !termux-clipboard-get
+# +
+import pathmagic
+with pathmagic.context():
+    from func.first import touchfilepath2depth, getdirmain
+    from termux_python.termux import battery_status
+    
+battery_status()
+# -
 
 
