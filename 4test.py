4c911c288854a736a544490c660eafd46a9da3fd
heart5 <baiyefeng@gmail.com>
2020-03-13 21:55:40 +0800

    启用ale插件，去掉无用的import

--- 4test.py
+++ 4test.py
@@ -2,10 +2,10 @@
 '''
 测试文件，啥都试试
 '''
-import func.first as first
+# import func.first as first
 from func.first import getdirmain
+
 
 if __name__ == '__main__':
     dm = getdirmain()
     print(dm)
-    # get_notestore()
