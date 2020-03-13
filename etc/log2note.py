41270c3c53bae2f9b9ab6e57eca7c91a1ac403cf
heart5 <baiyefeng@gmail.com>
2020-03-13 21:51:25 +0800

    autopep8发挥作用，自动排版

--- etc/log2note.py
+++ etc/log2note.py
@@ -25,6 +25,7 @@ with pathmagic.context():
 
 @timethis
 # @ift2phone()
+# @profile
 def log2note(noteguid, loglimit, levelstr='', notetitle='everwork日志信息'):
     namestr = 'everlog'
     cfp, cfppath = getcfp(namestr)
@@ -55,7 +56,8 @@ def log2note(noteguid, loglimit, levelstr='', notetitle='everwork日志信息'):
                                    for line in flog if line.find(levelstrinner) >= 0]
 
     ptn = re.compile('\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}')
-    tmlst = [pd.to_datetime(re.match(ptn, x).group()) for x in loglines if re.match(ptn, x)]
+    tmlst = [pd.to_datetime(re.match(ptn, x).group())
+             for x in loglines if re.match(ptn, x)]
     loglines = [x for x in loglines if re.match(ptn, x)]
     logsr = pd.Series(loglines, index=tmlst)
     logsr = logsr.sort_index()
@@ -76,7 +78,7 @@ def log2note(noteguid, loglimit, levelstr='', notetitle='everwork日志信息'):
         loglinesloglimit = loglines[(-1 * loglimit):]
         loglinestr = '\n'.join(loglinesloglimit[::-1])
         loglinestr = loglinestr.replace('<', '《').replace('>',
-            '》').replace('=', '等于').replace('&', '并或')        
+                                                          '》').replace('=', '等于').replace('&', '并或')
         loglinestr = "<pre>" + loglinestr + "</pre>"
         # print(loglinestr)
         try:
@@ -91,9 +93,10 @@ def log2note(noteguid, loglimit, levelstr='', notetitle='everwork日志信息'):
             log.critical(errmsg)
             ifttt_notify(errmsg, 'log2note')
 
+
 def log2notes():
     namestr = 'everlog'
-    device_id =getdeviceid()
+    device_id = getdeviceid()
     cfplog, cfplogpath = getcfp(namestr)
     if not cfplog.has_section(namestr):
         cfplog.add_section(namestr)
@@ -107,11 +110,13 @@ def log2notes():
         logguid = cfplog.get(device_id, 'logguid')
     else:
         note_store = get_notestore()
-        parentnotebook = note_store.getNotebook('4524187f-c131-4d7d-b6cc-a1af20474a7f')
+        parentnotebook = note_store.getNotebook(
+            '4524187f-c131-4d7d-b6cc-a1af20474a7f')
         evernoteapijiayi()
         note = ttypes.Note()
         note.title = f'服务器_{device_id}_日志信息'
-        notelog = makenote(token, note_store, note.title, notebody='', parentnotebook=parentnotebook)
+        notelog = makenote(token, note_store, note.title,
+                           notebody='', parentnotebook=parentnotebook)
         logguid = notelog.guid
         cfplog.set(device_id, 'logguid', logguid)
         cfplog.write(open(cfplogpath, 'w', encoding='utf-8'))
@@ -120,11 +125,13 @@ def log2notes():
         logcguid = cfplog.get(device_id, 'logcguid')
     else:
         note_store = get_notestore()
-        parentnotebook = note_store.getNotebook('4524187f-c131-4d7d-b6cc-a1af20474a7f')
+        parentnotebook = note_store.getNotebook(
+            '4524187f-c131-4d7d-b6cc-a1af20474a7f')
         evernoteapijiayi()
         note = ttypes.Note()
         note.title = f'服务器_{device_id}_严重错误日志信息'
-        notelog = makenote(token, note_store, note.title, notebody='', parentnotebook=parentnotebook)
+        notelog = makenote(token, note_store, note.title,
+                           notebody='', parentnotebook=parentnotebook)
         logcguid = notelog.guid
         cfplog.set(device_id, 'logcguid', logcguid)
         cfplog.write(open(cfplogpath, 'w', encoding='utf-8'))
@@ -146,14 +153,16 @@ def log2notes():
     if cfpfromnote.has_option(namestr, 'critical') and (cfpfromnote.getboolean(namestr, 'critical') == True):
         levelstrc = 'CRITICAL'
         # noteguidc = cfpeverwork.get('evernote', 'lognotecriticalguid')
-        log2note(logcguid, loglimitc, levelstrc, notetitle=f'服务器_{servername}_严重错误日志信息')
+        log2note(logcguid, loglimitc, levelstrc,
+                 notetitle=f'服务器_{servername}_严重错误日志信息')
 
     # noteguidn = cfpeverwork.get('evernote', 'lognoteguid')
-    log2note(noteguid=logguid, loglimit=loglimitc, notetitle=f'服务器_{servername}_日志信息')
+    log2note(noteguid=logguid, loglimit=loglimitc,
+             notetitle=f'服务器_{servername}_日志信息')
 
     # locinfo = termux_location()
     # print(locinfo)
- 
+
 
 if __name__ == '__main__':
     log.info(f'开始运行文件\t{__file__}')
