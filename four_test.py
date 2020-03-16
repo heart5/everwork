7708adb3a544e0e3db44ca03a85cc93aa76508a7
heart5 <baiyefeng@gmail.com>
2020-03-16 16:46:09 +0800

    为了测试的小程序

---
+++ four_test.py
@@ -0,0 +1,12 @@
+# encoding:utf-8
+'''
+测试文件，啥都试试
+'''
+# import func.first as first
+from func.first import getdirmain
+
+
+if __name__ == '__main__':
+    dm = getdirmain()
+    print(f"I am strong", end='\n\n')
+    print(dm)
