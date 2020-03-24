b19f09d177136e8626a8343a1e95835b88c879e4
heart5 <baiyefeng@gmail.com>
2020-03-25 00:16:41 +0800

    新增sha2hexstr，用hashlib代替hash，后者是和运行环境相关的，不是绝对相关。

--- func/sysfunc.py
+++ func/sysfunc.py
@@ -7,6 +7,7 @@ import os
 import sys
 import platform
 import uuid
+from hashlib import sha256
 # import wmi_client_wrapper as wmi
 
 import pathmagic
@@ -18,6 +19,16 @@ def uuid3hexstr(inputo: object):
     inputstr = str(inputo)
 
     return hex(hash(uuid.uuid3(uuid.NAMESPACE_URL, inputstr)))[2:].upper()
+
+
+def sha2hexstr(inputo: object):
+    if type(inputo) == bytes:
+        targetb = inputo
+    else:
+        targetb = str(inputo).encode('utf-8')
+    hhh = sha256(targetb)
+
+    return hhh.hexdigest().upper()
 
 
 def execcmd(cmd):
@@ -32,4 +43,5 @@ if __name__ == '__main__':
     outgetstr = execcmd("uname -a")
     print(outgetstr.strip("\n"))
     print(uuid3hexstr(outgetstr))
+    print(sha2hexstr(outgetstr))
     log.info(f'文件\t{__file__}\t测试完毕。')
