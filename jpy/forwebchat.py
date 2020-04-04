c26e80cbfcddf756b5552f76cb63eb49dd01ff46
heart5 <baiyefeng@gmail.com>
2020-04-04 13:28:51 +0800

    调试timeout功能函数

--- jpy/forwebchat.py
+++ jpy/forwebchat.py
@@ -202,8 +202,8 @@ with pathmagic.context():
 # lpwrapper = lp(showdelayimg(dbname))
 # lpwrapper()
 # lp.print_stats()
-men_wc = 'heart5'
-# men_wc = '白晔峰'
+# men_wc = 'heart5'
+men_wc = '白晔峰'
 dbname = getdirmain() / 'data' / 'db' / f"wcdelay_{men_wc}.db"
 
 showdelayimg(dbname)
