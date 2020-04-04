8dd557dc1d047ce145afd056e3cba91000063a30
heart5 <baiyefeng@gmail.com>
2020-04-04 13:31:10 +0800

    启动起来，调试各种termux-tools

--- termuxtest.py
+++ termuxtest.py
@@ -1,4 +1,4 @@
-t -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 # ---
 # jupyter:
 #   jupytext:
@@ -14,25 +14,102 @@ t -*- coding: utf-8 -*-
 #     name: python3
 # ---
 
+# ### 库准备
+
+# +
 import os
 import itchat
-from life.wcdelay import showdelayimg
+
+import pathmagic
+with pathmagic.context():
+    from func.first import dirmainpath
+    from life.wcdelay import showdelayimg
+    from func.termuxtools import *
+# -
+
+# ### termuxtools各种功能函数
+
+# 几乎都是硬件相关
+
+# #### 粘贴板
+
+termux_clipboard_get()
+
+# #### 手机联系人
+
+ctlst = termux_contact_list()
+
+type(ctlst)
+
+# +
+import re
+ptn = re.compile(u"{(.+)}", re.MULTILINE)
+ptn = re.compile(u"({.+?})", re.MULTILINE | re.DOTALL)
+re.findall(ptn, ctlst)[:3]
+ptnname = re.compile("\"name\": \"(.+?)\"", re.MULTILINE)
+ctnamelst = re.findall(ptnname, ctlst)
+ptnphone = re.compile("\"number\": \"(.+?)\"", re.MULTILINE)
+ctphonelst = re.findall(ptnphone, ctlst)
+len(list((zip(ctnamelst, ctphonelst))))
+len(dict((zip(ctnamelst, ctphonelst))))
+
+# print(ctlst)
+# -
+
+# #### 电池
+
+# ##### 【电池电量模块调用】
+
+# +
+import pathmagic
+with pathmagic.context():
+    from etc.battery_manage import getbattinfodb, showbattinfoimg
+    from func.first import touchfilepath2depth, getdirmain
+    
+dbnameouter = touchfilepath2depth(getdirmain() / "data" / "db" / f"batteryinfo.db")
+showbattinfoimg(dbnameouter)
+# getbattinfodb(dbnameouter)
+# -
+
+# ##### 各种调试
+
+bsdict = battery_status()
+bsdict
+bsdict['percentage']
+
+bsdict = battery_status()
+bsdict
+bsdict['percentage']
+
+import time
+perlst = list()
+while (bsdict := battery_status())['plugged'].upper() == 'PLUGGED_AC':
+    perlst.insert(0, bsdict['percentage'])
+    print(perlst)
+    if bsdict['percentage'] == 100:
+        break
+    time.sleep(20)
+
+# ### 启动微信发送图片
 
 itchat.auto_login(hotReload=True)   #热启动你的微信
 
+# + jupyter={"outputs_hidden": true}
 #查看你的群
 rooms=itchat.get_chatrooms(update=True)
-gor i in range(len(rooms[:2])):
+for i in range(len(rooms[:2])):
     for key, value in rooms[i].items():
         print(f"{key}\t{value}")
     print('\n')   
+# -
 
 #这里输入你好友的名字或备注。
 frd = itchat.search_friends(name=r'')  
 print(frd)
 username = frd['UserName']
 print(username)
-img = showdelayimg()
+dbname = dirmainpath / 'data' / 'db' / 'wcdelay_白晔峰.db'
+img = showdelayimg(dbname)
 print(img)
 
 print(img)
@@ -46,3 +123,5 @@ try:
     print("success")
 except Exception as e:
     print(f"fail.{e}")
+
+
