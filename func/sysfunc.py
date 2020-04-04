9f7836492f15c584fb0fb808f87f73bfac2c7f25
heart5 <baiyefeng@gmail.com>
2020-04-04 13:26:50 +0800

    增加timeout函数，避免特种情况下无限制无限时的硬件相关运行

--- func/sysfunc.py
+++ func/sysfunc.py
@@ -6,6 +6,8 @@
 import os
 import sys
 import platform
+import signal
+import time
 import uuid
 from hashlib import sha256
 # import wmi_client_wrapper as wmi
@@ -14,7 +16,42 @@ import pathmagic
 with pathmagic.context():
     from func.logme import log
 
+    
+def set_timeout(num, callback):
+    
+    def wrap(func):
+        def handle(signum, frame):  # 收到信号 SIGALRM 后的回调函数，第一个参数是信号的数字，第二个参数是the interrupted stack frame.
+            raise RuntimeError
 
+        def to_do(*args, **kwargs):
+            try:
+                if (sysstr := platform.system()) == "Linux":
+                    print(sysstr)
+                    signal.signal(signal.SIGALRM, handle)  # 设置信号和回调函数
+                    signal.alarm(num)  # 设置 num 秒的闹钟
+                    print('start alarm signal.')
+                    r = func(*args, **kwargs)
+                    print('close alarm signal.')
+                    signal.alarm(0)  # 关闭闹钟
+                    return r
+                else:
+                    r = func(*args, **kwargs)
+                    logstr = f"{sysstr}\t非linux系统，啥也没做。"
+                    log.warning(logstr)
+                    return r
+                    
+            except RuntimeError as e:
+                callback()
+
+        return to_do
+ 
+    return wrap
+
+
+def after_timeout():  # 超时后的处理函数
+    log.critical("运行超出预设时间，退出!")
+    
+    
 def uuid3hexstr(inputo: object):
     inputstr = str(inputo)
 
