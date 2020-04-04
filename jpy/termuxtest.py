212fa1db0eee206263ce0d70256454484cd7e404
heart5 <baiyefeng@gmail.com>
2020-04-05 02:46:13 +0800

    调试联系人模块

--- jpy/termuxtest.py
+++ jpy/termuxtest.py
@@ -19,11 +19,15 @@
 # +
 import os
 import itchat
+import pandas as pd
+import time
 
 import pathmagic
 with pathmagic.context():
-    from func.first import dirmainpath
+    from func.first import dirmainpath, touchfilepath2depth, getdirmain
     from life.wcdelay import showdelayimg
+    from life.phonecontact import getphoneinfodb
+    from func.pdtools import lststr2img
     from func.termuxtools import *
 # -
 
@@ -37,9 +41,53 @@ termux_clipboard_get()
 
 # #### 手机联系人
 
-ctlst = termux_contact_list()
+# ##### `showphoneinfoimg`函数
 
-type(ctlst)
+jujinm, ctdf = getphoneinfodb()
+jujinm
+ctdf
+
+# +
+ctdf.dtypes
+ctdf.shape[0]
+ctdf.groupby('appendtime').count().shape[0]
+contactoutstr = f"目前联系人共有{ctdf.shape[0]}个，有效添加次数为：{ctdf.groupby('appendtime').count().shape[0]}，最近一次添加时间为：{ctdf['appendtime'].max()}。\n"
+contactoutstr
+
+dayrange = 30
+descbegintime = pd.to_datetime(time.ctime()) + pd.Timedelta(f'-{dayrange}d')
+descbegintime
+
+contactoutstr += f"最近{dayrange}天添加的联系人如下(前20位）：\n"
+ctrecentstr = ctdf[ctdf.appendtime > descbegintime][-20:].to_string(justify='left', show_dimensions=True, index=False)
+contactoutstr += ctrecentstr
+contactoutstr
+# -
+
+lststr2img(contactoutstr, title="手机联系人综合描述", showincell=True)
+
+# ##### 联系人相关各种调试
+
+ctstr = termux_contact_list()
+
+type(ctstr)
+type(eval(ctstr))
+ctlst = eval(ctstr)
+ctlst[:5]
+ctlst[0].items()
+ctlst[0].values()
+
+# + jupyter={"outputs_hidden": true}
+[value.replace(' ', '') for item in ctlst for value in item.values()]
+# -
+
+ctdf = pd.DataFrame(ctlst)
+ctdf['number'] = ctdf['number'].apply(lambda x: x.replace(" ", ''))
+ctdf['appendtime'] = time.time()
+ctdf
+ctdf['appendtime'].max()
+
+ctdf.drop_duplicates('number')
 
 # +
 import re
@@ -56,19 +104,78 @@ len(dict((zip(ctnamelst, ctphonelst))))
 # print(ctlst)
 # -
 
+from collections import Counter
+namecounter = Counter(ctphonelst)
+{key:value for key, value in namecounter.items() if value > 1}
+
 # #### 电池
 
 # ##### 【电池电量模块调用】
 
 # +
+import pandas as pd
+import matplotlib.pyplot as plt
+
 import pathmagic
 with pathmagic.context():
     from etc.battery_manage import getbattinfodb, showbattinfoimg
     from func.first import touchfilepath2depth, getdirmain
-    
+
 dbnameouter = touchfilepath2depth(getdirmain() / "data" / "db" / f"batteryinfo.db")
+# -
+
 showbattinfoimg(dbnameouter)
 # getbattinfodb(dbnameouter)
+
+# ##### `showbattinfoimg`函数
+
+jujinm, battinfodf = getbattinfodb(dbnameouter)
+print(f"充电记录新鲜度：刚过去了{jujinm}分钟")
+jingdu=300
+
+# +
+plt.figure(figsize=(36, 12), dpi=jingdu)
+plt.style.use("ggplot")  # 使得作图自带色彩，这样不用费脑筋去考虑配色什么的；
+
+def drawdelayimg(pos, timedfinner, title):
+    # 画出左边界
+    tmin = timedfinner.index.min()
+    tmax = timedfinner.index.max()
+    shicha = tmax - tmin
+    bianjie = int(shicha.total_seconds() / 40)
+    print(f"左边界：{bianjie}秒，也就是大约{int(bianjie / 60)}分钟")
+    # plt.xlim(xmin=tmin-pd.Timedelta(f'{bianjie}s'))
+    plt.subplot(pos)
+    plt.xlim(xmin=tmin)
+    plt.xlim(xmax=tmax + pd.Timedelta(f"{bianjie}s"))
+    # plt.vlines(tmin, 0, int(timedf.max() / 2))
+#     plt.vlines(tmax, 0, int(timedfinner.max() / 2))
+
+    # 绘出主图和标题
+    plt.scatter(timedfinner.index, timedfinner, s=timedfinner)
+    plt.scatter(timedfinner[timedfinner == 0].index, timedfinner[timedfinner == 0], s=0.5)
+    plt.title(title, fontsize=40)
+    plt.tight_layout()
+
+timedf = battinfodf['percentage']
+drawdelayimg(321, timedf[timedf.index > timedf.index.max() + pd.Timedelta('-2d')], "电量%（最近两天）")
+plt.ylim(0, 110)
+drawdelayimg(312, timedf, "电量%（全部）")
+plt.ylim(0, 110)
+# imgwcdelaypath = touchfilepath2depth(getdirmain() / "img" / "hard" / "battinfo.png")
+
+timedf = battinfodf['temperature']
+drawdelayimg(322, timedf[timedf.index > timedf.index.max() + pd.Timedelta('-2d')], "温度℃（最近两天）")
+# ax = plt.gca()
+# ax.spines["bottom"].set_position(("data", 20))
+plt.ylim(20, 40)
+drawdelayimg(313, timedf, "温度℃（全部）")
+plt.ylim(20, 40)
+fig1 = plt.gcf()
+
+imgwcdelaypath = touchfilepath2depth(getdirmain() / "img" / "hard" / "batttempinfo.png")
+plt.show()
+fig1.savefig(imgwcdelaypath, dpi=jingdu)
 # -
 
 # ##### 各种调试
