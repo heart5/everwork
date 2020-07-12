60cb4e73574fc72c5639d4bb4c60716b2fa012c7
heart5 <baiyefeng@gmail.com>
2020-07-12 21:30:39 +0800

    autopep8排版优化

--- etc/battery_manage.py
+++ etc/battery_manage.py
@@ -28,7 +28,8 @@ def checkbatteryinfotable(dbname: str, tablename: str):
         print(batteryinfocreated)
         csql = f"create table if not exists {tablename} (appendtime int PRIMARY KEY, percentage int, temperature float)"
         ifnotcreate(tablename, csql, dbname)
-        setcfpoptionvalue('everhard', str(getdeviceid()), 'batteryinfodb', str(True))
+        setcfpoptionvalue('everhard', str(getdeviceid()),
+                          'batteryinfodb', str(True))
         logstr = f"数据表{tablename}在数据库{dbname}中构建成功"
         log.info(logstr)
 
@@ -48,7 +49,8 @@ def insertbattinfoitem2db(dbname: str, percentage: int, temperature: float):
         conn = lite.connect(dbname)
         cursor = conn.cursor()
         cursor.execute(
-            f"insert into {tablename} values(?, ?, ?)", (time.time(), percentage, temperature)
+            f"insert into {tablename} values(?, ?, ?)", (time.time(
+            ), percentage, temperature)
         )
 #         print(f"数据成功写入{dbname}\t{(timestampinput, elsmin)}")
         conn.commit()
@@ -59,10 +61,11 @@ def insertbattinfoitem2db(dbname: str, percentage: int, temperature: float):
         if conn:
             conn.close()
 
-            
+
 def batteryrecord2db(dbname: str):
     while (bsdict := battery_status())['plugged'].upper() == 'PLUGGED_AC':
-        insertbattinfoitem2db(dbname, bsdict['percentage'], bsdict['temperature'])
+        insertbattinfoitem2db(
+            dbname, bsdict['percentage'], bsdict['temperature'])
         time.sleep(30)
 
 
@@ -78,15 +81,17 @@ def getbattinfodb(dbname: str, tablename="battinfo"):
     cursor.execute(f"select * from {tablename}")
     table = cursor.fetchall()
     conn.close()
-    
-    timedf = pd.DataFrame(table, columns=["appendtime", "percentage", "temperature"])
+
+    timedf = pd.DataFrame(
+        table, columns=["appendtime", "percentage", "temperature"])
     timedf["appendtime"] = timedf["appendtime"].apply(
-            lambda x: pd.to_datetime(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(x))))
-    timedfgrp = timedf.set_index("appendtime")  
+        lambda x: pd.to_datetime(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(x))))
+    timedfgrp = timedf.set_index("appendtime")
 
     if (tdfsize := timedfgrp.shape[0]) != 0:
         print(f"电池记录共有{tdfsize}条")
-        jujinmins = int((pd.to_datetime(time.ctime()) - timedfgrp.index[-1]).total_seconds() / 60)
+        jujinmins = int((pd.to_datetime(time.ctime()) -
+                         timedfgrp.index[-1]).total_seconds() / 60)
     else:
         jujinmins = 0
         logstr = f"数据表{tablename}还没有数据呢"
@@ -125,26 +130,30 @@ def showbattinfoimg(dbname: str, jingdu: int = 300):
 
         # 绘出主图和标题
         plt.scatter(timedfinner.index, timedfinner, s=timedfinner)
-        plt.scatter(timedfinner[timedfinner == 0].index, timedfinner[timedfinner == 0], s=0.5)
+        plt.scatter(timedfinner[timedfinner == 0].index,
+                    timedfinner[timedfinner == 0], s=0.5)
         plt.title(title, fontsize=40)
         plt.tick_params(labelsize=20)
         plt.tight_layout()
 
     timedf = battinfodf['percentage']
-    drawdelayimg(321, timedf[timedf.index > timedf.index.max() + pd.Timedelta('-2d')], "电量（%，最近两天）")
+    drawdelayimg(321, timedf[timedf.index > timedf.index.max(
+    ) + pd.Timedelta('-2d')], "电量（%，最近两天）")
     plt.ylim(0, 110)
     drawdelayimg(312, timedf, "电量（%，全部）")
     plt.ylim(0, 110)
 #     imgwcdelaypath = touchfilepath2depth(getdirmain() / "img" / "hard" / "battinfo.png")
 
     timedf = battinfodf['temperature']
-    drawdelayimg(322, timedf[timedf.index > timedf.index.max() + pd.Timedelta('-2d')], "温度（℃，最近两天）")
+    drawdelayimg(322, timedf[timedf.index > timedf.index.max(
+    ) + pd.Timedelta('-2d')], "温度（℃，最近两天）")
     plt.ylim(20, 40)
     drawdelayimg(313, timedf, "温度（℃，全部）")
     plt.ylim(20, 40)
     fig1 = plt.gcf()
 
-    imgwcdelaypath = touchfilepath2depth(getdirmain() / "img" / "hard" / "batttempinfo.png")
+    imgwcdelaypath = touchfilepath2depth(
+        getdirmain() / "img" / "hard" / "batttempinfo.png")
 #     plt.show()
     fig1.savefig(imgwcdelaypath, dpi=jingdu)
     print(os.path.relpath(imgwcdelaypath))
@@ -153,12 +162,13 @@ def showbattinfoimg(dbname: str, jingdu: int = 300):
 
 
 if __name__ == "__main__":
-    logstrouter = "运行文件\t%s" %__file__
+    logstrouter = "运行文件\t%s" % __file__
     log.info(logstrouter)
-    dbnameouter = touchfilepath2depth(getdirmain() / "data" / "db" / f"batteryinfo.db")
+    dbnameouter = touchfilepath2depth(
+        getdirmain() / "data" / "db" / f"batteryinfo.db")
     # batteryrecord2db(dbnameouter)
     xinxian, tdf = getbattinfodb(dbnameouter)
     print(xinxian)
     print(tdf.sort_index(ascending=False))
-    logstrouter = "文件%s运行结束" %(__file__)
+    logstrouter = "文件%s运行结束" % (__file__)
     log.info(logstrouter)
