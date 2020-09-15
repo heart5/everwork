b1666ad09ff2adbfa7a1da86afc26da775392876
heart5 <ruguo1004g>
2020-09-15 23:31:55 +0800

    规范排版

--- etc/battery_recorder.py
+++ etc/battery_recorder.py
@@ -1,9 +1,10 @@
+# -*- coding: utf-8 -*-
 import pathmagic
 with pathmagic.context():
     from func.logme import log
     from func.first import touchfilepath2depth, getdirmain
     from etc.battery_manage import batteryrecord2db
-    
+
 if __name__ == "__main__":
     logstrouter = "运行文件\t%s" %__file__
     log.info(logstrouter)
