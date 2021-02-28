940d6fab0da911bb91fa550bf9ce17c4c60b6336
heart5 <baiyefeng@gmail.com>
2021-02-28 11:01:20 +0800

    import http, which is lost by me

--- func/evernttest.py
+++ func/evernttest.py
@@ -14,6 +14,7 @@ import traceback
 import inspect
 import numpy as np
 import pandas as pd
+import http
 from bs4 import BeautifulSoup
 from evernote.api.client import EvernoteClient
 from evernote.edam.error.ttypes import EDAMNotFoundException, EDAMSystemException, EDAMUserException, EDAMErrorCode
