174d66427886c8673dc7e995fabbf46366add613
heart5 <baiyefeng@gmail.com>
2021-03-09 16:26:53 +0800

    add a class sample

--- four_test.py
+++ four_test.py
@@ -5,8 +5,17 @@
 # import func.first as first
 from func.first import getdirmain
 
+class myobj(object):
+    def __init__(self):
+        print("I'm in a class.")
+
 
 if __name__ == '__main__':
     dm = getdirmain()
     print(f"I am strong", end='\n\n')
     print(dm)
+
+    mo = myobj()
+    print(mo)
+
+    print("Home is warmful.")
