28c02ca9bcd9caf5a05ab2deb2b272f941a6f238
heart5 <baiyefeng@gmail.com>
2021-05-10 16:36:38 +0800

    适应ipynb格式方便jupyter开发

--- etc/getip.py
+++ etc/getip.py
@@ -11,11 +11,19 @@
 #     name: python3
 # ---
 
+# %% [markdown]
+# # 获取IP地址
+
+# %%
 """
 获取ip地址
 """
-import socket
+# %% [markdown]
+# ## 库引入
 
+# %%
+import socket
+# %%
 def get_host_ip():
     """
     查询本机ip地址
@@ -25,11 +33,11 @@ def get_host_ip():
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         s.connect(('8.8.8.8', 80))
         sn = s.getsockname()
-        # print(sn)
+        print(sn)
         ip = sn[0]
     finally:
         s.close()
     return ip
-
+# %%
 if __name__ == '__main__':
     print(get_host_ip())
