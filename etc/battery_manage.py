607a43d8634e18360c1dd1fff0705eeed6333947
heart5 <baiyefeng@gmail.com>
2021-08-03 22:55:37 +0800

    使用新版本的jupytext

--- etc/battery_manage.py
+++ etc/battery_manage.py
@@ -2,12 +2,12 @@
 # ---
 # jupyter:
 #   jupytext:
-#     cell_metadata_filter: -all
 #     text_representation:
-#       extension: .py
-#       format_name: percent
-#       format_version: '1.3'
 #       jupytext_version: 1.10.3
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
 # ---
 
 # %%
