e9baad3b155c4b59868db1383600fb4310b9705f
heart5 <baiyefeng@gmail.com>
2021-08-03 22:19:10 +0800

    更改数据汇总机制，，主数据文件和笔记附件都放在数据目录下

--- func/ds2en.py
+++ func/ds2en.py
@@ -34,9 +34,14 @@ def huojieds2note():
     huojieguid = getinivaluefromnote('game', 'guid')
     notecontent = getnotecontent(huojieguid)
 
-    jushufromnote = int([x.split('：')[1] for x in notecontent.find('pre').text.split()][0])
+    neirong = notecontent.find('pre').text
+    print(neirong)
+    nrlst = neirong.split('\n')
+    print(nrlst)
+    jushufromnote = int(nrlst[0].split("：")[1])
+    print(jushufromnote)
     musedatapath = getdirmain() / 'data' / 'muse'
-    tlst = [musedatapath / pt for pt in os.listdir(musedatapath) if pt.endswith('xlsx') or pt.endswith('xls')]
+    tlst = [musedatapath / pt for pt in os.listdir(musedatapath) if (pt.endswith('xlsx') or pt.endswith('xls'))]
     df = pd.DataFrame()
     for datafile in tlst:
         df = df.append(pd.read_excel(datafile))
@@ -45,13 +50,13 @@ def huojieds2note():
     jushufromhost = df.shape[0] // 4
 
     if jushufromnote != jushufromhost:
-        datamainpath = getdirmain() / 'data' / 'tmp' / 'huojiemain.xlsx'
+        datamainpath = musedatapath / 'huojiemain.xlsx'
         touchfilepath2depth(datamainpath)
         
         reslst = getnoteresource(huojieguid)
         targetbinlst = [x[1] for x in reslst if x[0] == os.path.basename(datamainpath)]
         if len(targetbinlst) != 0:            
-            huojiefromnotepath = dirmainpath / 'data' / 'tmp' / 'huojiemainfromnote.xlsx'
+            huojiefromnotepath = musedatapath / 'huojiemainfromnote.xlsx'
             f = open(huojiefromnotepath, 'wb')
             f.write(targetbinlst[0])
             f.close()
@@ -67,7 +72,8 @@ def huojieds2note():
         deviceid = getdeviceid()
         if (device_name := getinivaluefromnote('device', deviceid)) is None:
             device_name = deviceid
-        updatereslst2note([datamainpath], huojieguid, neirong=f'局数：{jushufromhost}\n时间跨度：{timemin}至{timemax}\n{device_name}', filenameonly=True)
+        updatereslst2note([datamainpath], huojieguid, 
+                          neirong=f'局数：{jushufromhost}\n时间跨度：{timemin}至{timemax}\n主机名称：{device_name}', filenameonly=True)
     
     return jushufromnote, jusdhufromhost
 
@@ -84,5 +90,3 @@ if __name__ == '__main__':
 
     if not_IPython():
         log.info(f"文件\t{__file__}\t运行结束。")
-
-# %%
