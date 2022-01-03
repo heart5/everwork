0eab8bf7c40360b6223c32f3dbea3ad49a693608
heart5 <baiyefeng@gmail.com>
2022-01-04 01:53:30 +0800

    jupytext新版本适应

--- func/configpr.py
+++ func/configpr.py
@@ -1,4 +1,15 @@
 # encoding:utf-8
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       jupytext_version: 1.13.4
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
 """
 配置处理器(ConfigParser)相关的功能函数
 """
