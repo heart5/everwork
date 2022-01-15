273196869e188443930743d42ef957ecb0e0855b
heart5 <baiyefeng@gmail.com>
2022-01-15 14:36:40 +0800

    jupytext新版本1.13.4适应

--- func/pdtools.py
+++ func/pdtools.py
@@ -1,4 +1,15 @@
 # encoding:utf-8
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       jupytext_version: 1.13.4
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
 # %% [markdown]
 # # pandas应用函数库
 
