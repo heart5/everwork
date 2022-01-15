7a6a5f76ab714a668d8767ced40cb22f20712f2f
heart5 <baiyefeng@gmail.com>
2022-01-15 14:35:21 +0800

    jupytext新版本1.13.4适应

--- etc/linuxfontget.py
+++ etc/linuxfontget.py
@@ -3,12 +3,18 @@
 # jupyter:
 #   jupytext:
 #     text_representation:
-#       jupytext_version: 1.10.3
+#       jupytext_version: 1.13.4
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
 # ---
 
+# %%
 import matplotlib
 
+# %%
 ftypath = matplotlib.matplotlib_fname() #matplotlib配置文件路径名称
 print(ftypath)
 
-
+# %%
