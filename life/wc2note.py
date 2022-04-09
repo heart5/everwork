eb9ed92163be7b822269792a983d41a29e7bf0b8
heart5 <baiyefeng@gmail.com>
2022-04-09 20:54:32 +0800

    jupytext版本升级为1.3

--- life/wc2note.py
+++ life/wc2note.py
@@ -2,8 +2,11 @@
 # ---
 # jupyter:
 #   jupytext:
+#     notebook_metadata_filter: -jupytext.text_representation.jupytext_version
 #     text_representation:
-#       jupytext_version: 1.13.4
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
 #   kernelspec:
 #     display_name: Python 3 (ipykernel)
 #     language: python
@@ -533,17 +536,3 @@ if __name__ == '__main__':
 
     if not_IPython():
         log.info(f"文件\t{__file__}\t运行结束。")
-
-# %%
-mydict = alldfdesc2note(wcpath)
-
-# %%
-name = 'heart5'
-df = mydict[name]
-df.dtypes
-
-# %%
-df.describe()
-
-# %%
-df.sort_values(['time'])
