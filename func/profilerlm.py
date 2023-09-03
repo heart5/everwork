5ac551d86893a700c83cb29bb9b73398b5b7f850
heart5 <baiyefeng@gmail.com>
2023-09-03 19:27:27 +0800

    应用新的jupytext配置，减少kernelspec的元信息显示保存

--- func/profilerlm.py
+++ func/profilerlm.py
@@ -1,4 +1,16 @@
 # encoding:utf-8
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     formats: ipynb,py:percent
+#     notebook_metadata_filter: jupytext,-kernelspec,-jupytext.text_representation.jupytext_version
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+# ---
+
 # %%
 """
 装饰器函数集，ift2phone、timethis, logit
