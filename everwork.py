3c7b56fa8f1d36605b52473de12e73319854ce83
heart5 <baiyefeng@gmail.com>
2023-11-20 13:36:53 +0800

    适应jupytext

--- everwork.py
+++ everwork.py
@@ -2,16 +2,13 @@
 # ---
 # jupyter:
 #   jupytext:
+#     cell_metadata_filter: -all
 #     formats: ipynb,py:percent
+#     notebook_metadata_filter: jupytext,-kernelspec,-jupytext.text_representation.jupytext_version
 #     text_representation:
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.3'
-#       jupytext_version: 1.5.1
-#   kernelspec:
-#     display_name: Python 3
-#     language: python
-#     name: python3
 # ---
 
 # %%
